f6392e19fbce4ce9013743fa8fbd1a7ed032f349
Steve McLoughlin <steve.mcloughlin@hotmail.co.uk>
2020-03-20 17:15:57 +0000

    adding dynamic data

---
+++ src/_data/news.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+require('dotenv').config();
+
+module.exports = async function() {
+    try {
+      const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=gb&apiKey=${process.env.API_KEY}`);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+}
